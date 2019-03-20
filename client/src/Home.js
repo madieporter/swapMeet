e8c261c4ca6092b03f9becb4ca7504d86cbf4453
Madie Porter <madieporter@gmail.com>
2019-03-20 14:00:54 -0600

    push name

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,41 @@
 import React , { Component } from "react";
+import { withServices } from "./ServiceProvider";
 
-const Home = () => {
-    return (
-        <div>
 
-        </div>
-    )
+class Home extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            input: "",
+            filteredBeers: []
+        }
+    }
+
+    handleChange = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        //SET UP FILTER HERE
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text"
+                    placeholder="Search Swappers"
+                    name="searchSwappers"
+                    value={this.state.name}
+                    onChange={this.handleChange}/><br></br>
+                    <button>SEARCH</button>
+                </form>
+            </div>
+        )
+    }
 }
 
-export default Home;
+export default withServices(Home);
