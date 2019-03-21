0466db1600ef40bf23caaf0161e011420194deba
Sunny Chanthavong <sudochanth@gmail.com>
2019-03-21 08:41:43 -0600

    push

--- client/src/DisplayServices.js
+++ client/src/DisplayServices.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Profile from './Profile'
 
-const DisplayServices = (props) => {
-  let { minCost, maxCost, service, serviceType } = props.result
-  let { firstName, lastName, businessName, city, state } = props.result.swapper[0]
-  console.log(props.result)
-  return (
-    <div>
+import React, { Component } from 'react'
+
+export default class DisplayServices extends Component {
+  constructor() {
+    super()
+
+  }
+
+  //function to go to profile. send results to function
+  // () => {<Profile result={this.props.result}
+  //
+  render() {
+    let { minCost, maxCost, service, serviceType } = props.result
+    let { firstName, lastName, businessName, city, state } = props.result.swapper[0]
+    let result = props.result
+    return (
+      <div onClick={}>
       <div>{serviceType}</div>
       <div>{service}</div>
       <div>{firstName} {lastName}</div>
@@ -13,13 +25,14 @@ const DisplayServices = (props) => {
       <div>{businessName}</div>
       <div>{city}, {state}</div>
     </div>
-  )
+    )
+  }
 }
 
-export default DisplayServices;
 
 // sunny
-//every prop in its own div (except f/l name, cost)
+// every prop in its own div (except f/l name, cost)
 // profile comp shows everything (except id, v)
+// on click on parent div, set props in thing
 
 // img service type ternary
