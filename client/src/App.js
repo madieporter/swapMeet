b243f619f0b9219e28d0d07ac92926c4be65bb95
Madie Porter <madieporter@gmail.com>
2019-03-21 08:49:22 -0600

    navbar updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { Switch, Route } from "react-router-dom";
+
 import './App.css';
 
-import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home.js";
+import Auto from "./Auto.js";
+import Crafts from "./Crafts.js";
+import Education from "./Education";
+import Events from "./Events";
+import ResidentialandCommercial from "./ResidentialAndCommercial";
+import Wellness from "./Wellness";
 
 
 
@@ -9,8 +18,16 @@ class App extends Component {
   render() {
     return (
       <div>
-       <Switch>
+        <NavBar />
 
+       <Switch>
+          <Route exact path = "/" component={Home}/>
+          <Route path="/auto" component={Auto}/>
+          <Route path="/crafts" component={Crafts}/>
+          <Route path="/education" component={Education}/>
+          <Route path="/events" component={Events}/>
+          <Route path="/residentialandcommercial" component={ResidentialandCommercial}/>
+          <Route path="/wellness" component={Wellness}/>
        </Switch>
       </div>
     );
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import menuBtn from "./menuBtn.png";
+
+
 const NavBar = () => {
+
     return (
         <div>
-            <div>
-                <link />
-            </div>
+            <header className="navBar ">
+                <nav className="navigation">
+                    <div></div>
+                    <div className="navLogo"><a href="/"><img src={menuBtn}/></a></div>
+                    <div className="navItems"> 
+                        <Link to="/">Home</Link>
+                        <Link to="/auto">Auto</Link>
+                        <Link to="/crafts">Crafts</Link> 
+                        <Link to="/education">Education</Link>
+                        <Link to="/events">Event Coordination</Link>
+                        <Link to="/residentialandcommercial">Residental and Commercial</Link>
+                        <Link to="/wellness">Wellness</Link>
+                    </div> 
+                </nav>
+            </header>
+            
         </div>
     )
-}
+} 
 
 export default NavBar;
