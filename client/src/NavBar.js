6e9c32f9874407723be30dc38435a3239b0fd580
Stephanie Lane <stephlane544@gmail.com>
2019-03-25 17:56:23 -0600

    aksdjgh

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -6,18 +6,26 @@ import { withRouter } from 'react-router-dom'
 import swapmeetLogo from "./images/swapmeetLogo.png";
 import login from "./images/login.png";
 
-import React, { Component } from 'react';
 
 class NavBar extends Component {
+  
+    toLogin = () => {
+        if(this.props.token){
+            this.props.history.push(`/userprofile/${this.props.user.username}`)
+        }else{
+            this.props.history.push(`/login`)
+        }
+    }
 
     toHome = () => {
-        this.props.history.push()
+        this.props.history.push(`/`)
     }
+
     render() {
         return (
             <div>
                 <nav className="navigation">
-                    <DrawerToggleButton click={props.drawerClickHandler} />
+                    <DrawerToggleButton click={this.props.drawerClickHandler} />
                     <img className="logo" src={swapmeetLogo} onClick={this.toHome} alt=""/>
                     <div className="loginOnHome">
                         <img className="navItems" src={login} alt=""/><br></br>
